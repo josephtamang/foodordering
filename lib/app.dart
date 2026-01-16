57065c6334f7e4866a94b0f01040dc871eb555e9
Joseph <Jasephname12345@gmail.com>
2026-01-16 08:28:26 +0545

    folders created

--- lib/app.dart
+++ lib/app.dart
@@ -8,6 +8,9 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'Poppins',
+      ),
       home: const SplashScreen(),
     );
   }
